33ab31d52fca2dd851797e8f3e72a62faa873034
MooseMan <ssaciolo@gmail.com>
2019-12-18 19:33:29 -0500

    Pushin Project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react"
 
-import "./App.css";
+import "./App.css"
+import Form from "./Form"
+import Team from "./Team"
 
 function App() {
+  const [teamMember, setTeamMember] = useState([
+    {
+      name: "Steve",
+      email: "email@email.com",
+      role: "Full Stack Web Developer"
+    }
+  ])
+
+  const [memberToEdit, setMemberToEdit] = useState([])
+
+  const editMember = item => {}
+
+  console.log(teamMember)
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Form teamMember={teamMember} setTeamMember={setTeamMember} />
+      {teamMember.map(item => {
+        return <Team teamMember={item} />
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
